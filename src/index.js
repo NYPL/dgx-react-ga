33f335b16605fc69b0881b997f518f929bc5f2ec
Kang Peng <kangtingpeng@nypl.org>
2016-08-11 18:22:10 -0400

    Removed redundant pageview sent.

--- src/index.js
+++ src/index.js
@@ -40,10 +40,6 @@ function GaUtils() {
   this._setPublicPreviewDimension = () => {
     return ga.ga('set', 'dimension1', 'Public Preview');
   };
-
-  this._sendPublicPreviewDimension = () => {
-    return ga.ga('send', 'pageview');
-  };
 }
 
 export default {
