05a5a2855ca03ec5e027e97020d3ca34903ac9d4
Gregory Kallenberg <gregorykallenberg@nypl.org>
2020-05-13 09:18:40 -0400

    attempt at fixing window object error

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     filename: 'index.min.js',
     libraryTarget: 'umd',
     library: 'dgxReactGa',
-    globalObject: "window",
+    globalObject: "this",
   },
   externals: {
     'react': {
